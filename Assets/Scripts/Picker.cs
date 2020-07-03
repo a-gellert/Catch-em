9c14a3e8163304a1e441bb6b52e08986f1a3c35d
Александр Геллерт <sever6131@ya.ru>
2020-07-03 16:51:23 +0600

    added sound

--- Assets/Scripts/Picker.cs
+++ Assets/Scripts/Picker.cs
@@ -4,22 +4,17 @@ using UnityEngine;
 
 public class Picker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
+    public AudioSource chpockSound;
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (!StateManager.isEndGame && Input.GetMouseButtonDown(0))
         {
             Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
 
             if (hit.collider != null && hit.collider.tag == "Ball")
             {
+                chpockSound.Play();
                 hit.collider.gameObject.GetComponent<Ball>().ResBall();
                 StateManager.currentCount += hit.collider.gameObject.GetComponent<Ball>().count;
             }
--- Assets/Scripts/StateManager.cs
+++ Assets/Scripts/StateManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StateManager : MonoBehaviour
 {
@@ -11,17 +12,17 @@ public class StateManager : MonoBehaviour
     private TextMeshProUGUI current;
     [SerializeField]
     private TextMeshProUGUI record;
-
     public static int currentCount = 0;
     public static int recordCount;
-    public static int time = 60;
+    public static int time;
+    public static bool isEndGame = false;
     private void Start()
     {
+        time = 60;
         SaveLoad.LoadFile();
         StartCoroutine(Timer());
     }
 
-    // Update is called once per frame
     private void Update()
     {
         current.SetText(currentCount.ToString());
@@ -33,9 +34,16 @@ public class StateManager : MonoBehaviour
         }
         if (time == 0)
         {
+            timer.enabled = false;
+            current.enabled = false;
             SaveLoad.SaveFile();
-            Time.timeScale = 0;
+            isEndGame = true;
         }
+    }
+    public void Restart()
+    {
+        isEndGame = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
     private IEnumerator Timer()
     {
